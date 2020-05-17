8fe483ab82c04f0c033996e30698e39abec56d83
Giovanni Armane <giovanniarmane@gmail.com>
2020-05-17 19:26:02 -0300

    feat(src/entities/systems/weapons_system): add ready timer to laser beam

--- src/entities/systems/weapons_system/WeaponsSystem.gd
+++ src/entities/systems/weapons_system/WeaponsSystem.gd
@@ -4,11 +4,14 @@ extends Node2D
 export (PackedScene) var laser_beam
 export var laser_speed := 0
 
+onready var laser_ready_timer: Timer = $LaserReady
 onready var muzzle: Position2D = $Muzzle
 onready var root := get_tree().get_root()
 
 
 func shoot_laser_beam():
-	var beam = laser_beam \
-		.instance().initialize(muzzle.global_position, laser_speed)
-	root.add_child(beam)
+	if laser_ready_timer.is_stopped():
+		var beam = laser_beam \
+			.instance().initialize(muzzle.global_position, laser_speed)
+		root.add_child(beam)
+		laser_ready_timer.start()
