4e59b5a1e787080754940cc3f0f35cf13325b185
Giovanni Armane <giovanniarmane@gmail.com>
2020-05-31 22:08:27 -0300

    refactor(src/scenes/tests/gameplay01/background): change property name from velocity to speed

--- src/scenes/tests/Gameplay01/Background.gd
+++ src/scenes/tests/Gameplay01/Background.gd
@@ -1,9 +1,9 @@
 extends ParallaxBackground
 
 
-export var velocity := 150
+export var speed := 150
 
 
 func _process(delta: float) -> void:
-	var offset = get_scroll_offset().y + velocity * delta
+	var offset = get_scroll_offset().y + speed * delta
 	set_scroll_offset(Vector2(0, offset))
