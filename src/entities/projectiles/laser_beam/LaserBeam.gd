2eda5cb2372e2301bbcfca8117ebc6573111520e
Giovanni Armane <giovanniarmane@gmail.com>
2020-06-04 18:41:09 -0300

    improvement(src/entities/projectiles/laser_beam): add function to remove entity when out of screen

--- src/entities/projectiles/laser_beam/LaserBeam.gd
+++ src/entities/projectiles/laser_beam/LaserBeam.gd
@@ -5,12 +5,7 @@ onready var animated_sprite := $AnimatedSprite
 export var speed := 750
 
 
-func initialize(init_pos: Vector2, init_speed: int) -> Area2D:
-	position = init_pos
-	speed = init_speed
-	return self
-
-
+# Callbacks
 func _physics_process(delta):
 	var velocity = Vector2(0, -speed) * delta
 	position += velocity
@@ -20,6 +15,18 @@ func _ready():
 	animated_sprite.play()
 
 
+# Public API
+func initialize(init_pos: Vector2, init_speed: int) -> Area2D:
+	position = init_pos
+	speed = init_speed
+	return self
+
+
+# Signal handlers
 func _on_LaserBeam_body_entered(body: Node) -> void:
 	body.hit(self)
+	queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
