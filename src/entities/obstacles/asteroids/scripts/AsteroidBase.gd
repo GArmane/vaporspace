52e049bd912a97bd5e2a0df32f6109069084d351
Giovanni Armane <giovanniarmane@gmail.com>
2020-06-04 18:27:14 -0300

    refactor(src/entities/obstacles/asteroids/scripts/asteroidbase.gd): define remove function
    Define remove function so it is possible to delete an Asteroid separate from object gameplay
    destruction logic

--- src/entities/obstacles/asteroids/scripts/AsteroidBase.gd
+++ src/entities/obstacles/asteroids/scripts/AsteroidBase.gd
@@ -8,10 +8,14 @@ export var points := 10
 
 
 # Public API
-func destroy():
+func destroy() -> void:
 	emit_signal("destroyed", points)
-	queue_free()
+	remove()
 
 
-func hit(hitter: Node) -> void:
+func hit(_hitter: Node) -> void:
 	destroy()
+
+
+func remove() -> void:
+	queue_free()
