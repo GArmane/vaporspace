6ba14d1b28b858d55d42f3d3488f1590efd41736
Giovanni Armane <giovanniarmane@gmail.com>
2020-07-06 21:53:25 -0300

    feat(src/scenes/main): add game timer to main scene

--- src/scenes/main/GameController.gd
+++ src/scenes/main/GameController.gd
@@ -4,20 +4,40 @@ extends Node2D
 onready var asteroid_spawner: Position2D = $AsteroidSpawner
 onready var gui: MarginContainer = $CanvasLayer/GUI
 onready var game_over_ui: Control = $CanvasLayer/GameOverUI
+
+onready var game_timer: Timer = $Timers/GameTimer
 onready var reload_timer: Timer = $Timers/ReloadTimer
-onready var start_timer: Timer = $Timers/StartTimer
+onready var wave_timer: Timer = $Timers/WaveTimer
 
 
 onready var score := 0
+export var game_time := 170
+export var time_to_reload_after_death := 5
+export var time_to_reload_after_end := 8
+export var time_between_waves := 3
+
+
+# Private API
+func _reload_scene() -> void:
+	var _res = get_tree().reload_current_scene()
+
+
+func _end_game(restart_time: int) -> void:
+	game_over_ui.show()
+	wave_timer.stop()
+	reload_timer.wait_time = restart_time
+	reload_timer.start()
 
 
 # Callbacks
 func _ready() -> void:
 	# Prepare UI
-	game_over_ui.hide()
 	gui.update_score(score)
-
-	start_timer.start()
+	# Prepare timers
+	game_timer.wait_time = game_time
+	wave_timer.wait_time = time_between_waves
+	# Start game
+	wave_timer.start()
 
 
 # Signal handlers
@@ -26,16 +46,19 @@ func _on_Asteroid_destroy(value: int) -> void:
 	gui.update_score(score)
 
 
+func _on_GameTimer_timeout():
+	_end_game(time_to_reload_after_end)
+
+
 func _on_StartTimer_timeout() -> void:
 	asteroid_spawner.spawn_wave(self, "_on_Asteroid_destroy", 0.5, 10)
 	yield(asteroid_spawner, "wave_end")
-	start_timer.start()
+	wave_timer.start()
 
 
 func _on_Player_dead():
-	game_over_ui.show()
-	reload_timer.start()
+	_end_game(time_to_reload_after_death)
 
 
 func _on_ReloadTimer_timeout() -> void:
-	var _res = get_tree().reload_current_scene()
+	_reload_scene()
